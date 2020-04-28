2ebe313ec5f94fd3024efcafc99d70968f050784
Deshanm123 <deshanm123@gmail.com>
2020-04-28 22:46:18 +0530

    included  few functions
    countdown function and move mole function

--- script.js
+++ script.js
@@ -4,34 +4,50 @@ const timeLeft = document.querySelector('#time-left');
 let score = document.querySelector('#score');
 
 let result = 0;
+let currentTime = timeLeft.textContent;
 
-function randomSquare(){
+function randomSquare() {
 //removing mole function from all of squares
-     square.foreach(ClassName => {
-         className.classList.remove('mole');
-     })
+     square.forEach(className => {
+         //removal of class from html,which results removal of css effects
+         className.classList.remove("mole");
+     }); 
      
 //selecting a random square and assigning mole
-     let randomPosition = square[ Math.floor(Math.random()*9) ];
+//math.random generate value from 0 to 1 inclusive
+     let randomPosition = square[Math.floor(Math.random() * 9)];
      randomPosition.classList.add('mole');
  
-    let hitPosition = randomPosition.id;
- }
+     hitPosition = randomPosition.id;
+    }
 
- // if hit or click with mouse
+ // if hit with mouse on mole gerenerate a scor e 
 square.forEach(id => {
      id.addEventListener('mouseup', () => {
          if(id.id === hitposition){
              result+=1;
-             score.textContent =result;
+             score.textContent =  result;
          } 
-     })
-})
+     });
+});
 
 function moveMole() {
     
     let timerId = null;
     timerId = setInterval(randomSquare,1000);
 }
+//reducing time
+function countDown(){
+    currentTime--;
+    timeLeft.textContent=currentTime;
+    if (currentTime===0){
+        clearInterval(timerId);
+    alert(`Game over!!
+        you have scored ${result}`);
+    }
 
+}
+//passing counter Id as 1 sec
+let timerId= setInterval(countDown,1000);
+randomSquare();
 moveMole();   
